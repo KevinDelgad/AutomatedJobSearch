06182fc55283655b22de68210b74b4609c625552
KevinDelgad <kevindel891@gmail.com>
2023-02-19 14:27:24 -0800

    minor changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from allRecipesResultPage import list_Recipe
+from allRecipesResultPage import list_All_Recipe, list_Valid_All_Recipes
+from FoodNetworkResults import list_valid_Fn_Recipes
 
 if __name__ == "__main__":
-    url = "https://www.allrecipes.com/recipe/8509102/chicken-al-pastor/"
-    list_Recipe(url)
+    url = "https://www.foodnetwork.com/search/tacos-"
+    list_valid_Fn_Recipes(url)
