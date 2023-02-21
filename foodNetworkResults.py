17d7eecf97a579e3aae50ef4c7ac03ef8c28d3fd
KevinDelgad <kevindel891@gmail.com>
2023-02-21 12:15:41 -0800

    add function to grab and print recipe ingredients

--- foodNetworkResults.py
+++ foodNetworkResults.py
@@ -16,3 +16,13 @@ def list_valid_fn_recipes(url):
         link_url = recipe.find_element(By.TAG_NAME, 'a').get_attribute('href')
         print(recipe.text)
         print(link_url)
+
+def list_fn_recipe(url):
+    opts = ChromeOptions()
+    opts.add_argument("--headless")
+    browser = Chrome(options=opts)
+    browser.get(url)
+    ingredients = browser.find_elements(By.CLASS_NAME, "o-Ingredients__a-Ingredient")
+    for ingredient in ingredients:
+        if(ingredient.text != "Deselect All"):
+            print(ingredient.text)
